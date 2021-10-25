3e12df972e846e618e94ad1265ff44c08eb77b42
Ailye <ph.meillot@gmail.com>
2021-10-25 15:29:10 +0200

    fin de quete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_LAUNCHES = gql`
+  query GetLaunches {
+    launches(limit: 5) {
+      launch_date_utc
+      launch_success
+      rocket {
+        rocket_name
+      }
+      links {
+        video_link
+      }
+      details
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(GET_LAUNCHES);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
   return (
     <div className="App">
       <header className="App-header">
